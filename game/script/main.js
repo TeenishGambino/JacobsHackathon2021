404b7fae8b7700d219167b79bdcd52f4c70de02d
Sauravraut <saurav2raut@gmail.com>
2021-11-21 06:10:02 +0100

    adding texture

--- game/script/main.js
+++ game/script/main.js
@@ -1,19 +1,29 @@
-const back1 = new Image();
-back1.src = "./img/back.png";
+// const back1 = new Image();
+// back1.src = "./img/back.png";
 const back2 = new Image();
 back2.src = "./img/mid.png";
 const back3 = new Image();
 back3.src = "./img/front.png";
-const player_img = new Image();
-player_img.src = "./img/player_happy.png"
 
-var layer1 = new layer(back1,0.25);
+const wave_img = new Image();
+wave_img.src = "./img/tsunami1.png";
+const wave_img2 = new Image();
+wave_img2.src = "./img/tsunami2.png"
+
+const shouse1 = new Image();
+shouse1.src = "./img/shouse1.png";
+const large_house = new Image();
+large_house.src = "./img/largehouse.png";
+// var layer1 = new layer(back1,0.25);
 var layer2 = new layer(back2,0.5);
 var layer3 = new layer(back3,0.75);
+play = 1;
+const player_img = new Image();
+player_img.src = "./img/player_happy_r.png"
 
 var player = {
-    x:30,
-    y:170,
+    x:400,
+    y:250,
     h:100,
     w:100,
     vy:-5,
@@ -21,20 +31,50 @@ var player = {
         ctx.drawImage(player_img, player.x, ctx.canvas.height - player.y, player.h, player.w);
     }
 };
+var wave = {
+    x:-400,
+    y:50,
+    h:600,
+    w:300,
+    s_mod:2,
+    draw:function(){
+        this.x += 1 * this.s_mod;
+        this.y += 0.5 * this.s_mod;
+        //ctx.fillRect(wave.x,ctx.canvas.height - wave.y,wave.h,wave.w)
+        ctx.drawImage(wave_img, wave.x, ctx.canvas.height - wave.y, wave.h, wave.w);
+    }
+};
 
 var grounded = 0; //flag for if player can jump
 
 //add new objects belows this
-var ground = new rec(0,100,500,100);
-var ground2 = new rec(600,100,800,100);
-var ground3 = new rec(1400,200,400,200);
-var ground4 = new rec(1900,100,300,100);
-var ground5 = new rec(2250,50,100,50);
-var ground6 = new rec(2400,100,100,100);
+//max jump ~ 170
+//var ground = new rec(large_house,0,100,200,100);
+var ground = new rec(-500,100,900,100);
+// var ground2 = new rec(650,200,400,200);
+// var ground3 = new rec(1050,300,350,300);
+// var ground4 = new rec(1400,350,300,350);
+// var ground5 = new rec(2000,350,500,50);
+// var ground6 = new rec(1900,90,500,90);
 //above this 
+var collectables = [{
+    x:1600,
+    y:420,
+    h:100,
+    w:100
+},{
+    x:1600,
+    y:220,
+    h:100,
+    w:100
+}];
 
 function update(){// update loop runs 30 times a sec
-   
+    if(collision(wave,player) || player.y <= 0){
+        alert("You lost to the wave!");
+        play = 0;
+    }
+
     //SOMEHOW WORKS PLZ DONT TOUCH
     
     ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height); //clears canvas
@@ -52,36 +92,35 @@ function update(){// update loop runs 30 times a sec
     player.vy -= (player.vy < -5) ? 0 : 5;
 
     if(right){
+        player_img.src = "./img/player_happy_r.png"
         for(layer of layers_arr){
-            console.log(layer.x);
              layer.update(-1);
-            console.log(layer.x);
         }
         for (i of obj_arr){
             i.x -= hori_speed;
         }
     }
     if(left){
-        if(obj_arr[0].x < 0){
-            for(layer of layers_arr){
-                layer.update(1);
-            }
-            for (i of obj_arr){
-                i.x += hori_speed;
-            }
+        player_img.src = "./img/player_happy_l.png"
+        for(layer of layers_arr){
+            layer.update(1);
+        }
+        for (i of obj_arr){
+            i.x += hori_speed;
         }
     }
     for(i of obj_arr){
-        if(!collision(i, player)){ // returns 1 if player does not collide with 'i'
-            
-        }else{ 
-            player.y = i.y + player.h;
+        if(collision(i, player)){ // returns 1 if player does not collide with 'i'
+            //player.y = i.y + player.h;
             player.vy = 0;
             grounded = 1;
         }
         i.draw();
+        wave.draw();
     }
     player.draw();
 }
 
-setInterval(update,1000/30); 
+if(play){
+    setInterval(update,1000/30); 
+}
